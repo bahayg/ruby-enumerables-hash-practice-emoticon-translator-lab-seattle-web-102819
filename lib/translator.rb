e333cec1c5b64359a414c8e3d9c43968ccdd21fe
Bahay Gulle Bilgi <bahayg@gmail.com>
2019-10-09 01:14:42 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,7 @@
 require YAML
 
 def load_library (file_path)
-  emoticons = YAML.load_file(file_path
+  emoticons = YAML.load_file(file_path)
   new_hash = {"get_meaning" => {}, "get_emoticon" => {}}
   emoticons.each do |key, value|
     new_hash["get_emoticon"][value[0]] = emoticons[key][1]
